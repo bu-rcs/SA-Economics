1cdb3213ee5eec7c991cbc659af7bf57b159cd38
bu-bgregor <bgregor@bu.edu>
2021-07-21 13:42:44 -0400

    Update batch_example.sh

--- Stata_codes/batch_example.sh
+++ Stata_codes/batch_example.sh
@@ -1,8 +1,21 @@
-#!/bin/sh 
+#!/bin/bash -l 
 
+# Set the run time limit for the job. The default is 12 hours.
+# There is no advantage in queue scheduling when requesting
+# <12 hours, but as an example this job is set to have a 2 hour
+# limit. 
 #$ -l h_rt=2:00:00
+
+# Email yourself when the job finishes.
 #$ -m e
+
+# Join the console output and error files. This is optional.
 #$ -j y 
+
+# Give the job a name, the console output file will be named
+# econ_stata_demo.oXYZ where XYZ is the job number.
+#$ -N econ_stata_demo
+
 
 echo "==========================================================" 
 echo "Starting on : $(date)"
@@ -13,7 +26,10 @@ echo "Current job name : $JOB_NAME"
 echo "Task index number : $SGE_TASK_ID"
 echo "==========================================================" 
 
-module load stata
+# Run this command to see the available versions of stata:
+#    module avail stata
+# stata/17 and stata-se/17 are the same thing - single core STATA.
+module load stata/17
 
 stata -b do do_example.do
 
